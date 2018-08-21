583c2dc0afb779f8c024646ad45dfba95e6b3685
Somesh K Bhaskar <Somesh.Bhaskar@Diaspark.com>
2018-08-21 11:45:35 +0530

    Grid Template Updated and Basic Design Block Generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { TestimonialComponent } from './shared/testimonial/testimonial.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { CarouselComponent } from './shared/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TestimonialComponent,
+    BannerComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
